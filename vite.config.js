65f760b093880337cf89e04f036675a713dc0d06
Max <scorpiomax90@gmail.com>
2023-09-24 15:32:16 +0300

    added multi-page functional

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import imagemin from 'imagemin'
 import imageminWebp from 'imagemin-webp'
+import path from 'path'
 import { defineConfig } from 'vite'
+import glob from 'fast-glob'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [
     {
-			...imagemin(['./src/img/*.{jpg,png,jpeg}'], {
+			...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
 				destination: './src/img/webp/',
 				plugins: [
 					imageminWebp({ quality: 50 })
@@ -14,4 +17,14 @@ export default defineConfig({
 			apply: 'serve',
 		}
   ],
+	build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+				glob.sync(['./*.html', './pages/**/*.html']).map(file => [
+					path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
+					fileURLToPath(new URL(file, import.meta.url))
+				])
+			)
+    },
+  },
 })
