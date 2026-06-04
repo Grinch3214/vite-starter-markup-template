1a789dbb99c1d5b4501291eb2989912793a446f2
Max <scorpiomax90@gmail.com>
2026-06-04 20:04:00 +0300

    add handlebars HMR plugin and update readme

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import imagemin from 'imagemin'
 import imageminWebp from 'imagemin-webp'
 import path from 'path'
+import fs from 'fs'
 import { defineConfig } from 'vite'
 import glob from 'fast-glob'
 import { fileURLToPath } from 'url'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import handlebars from 'vite-plugin-handlebars'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -14,6 +16,21 @@ const rootPath = '/'
 
 export default defineConfig({
 	plugins: [
+		handlebars({
+			partialDirectory: path.resolve(__dirname, 'src/partials'),
+			context(pagePath) {
+				const dataFile = path.resolve(
+					__dirname,
+					'src/data',
+					pagePath.replace(/^\//, '').replace('.html', '.json')
+				)
+				try {
+					return JSON.parse(fs.readFileSync(dataFile, 'utf-8'))
+				} catch {
+					return {}
+				}
+			},
+		}),
 		ViteImageOptimizer({
 			svg: {
 				plugins: [
@@ -41,6 +58,21 @@ export default defineConfig({
 				await imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
 					destination: './src/img/webp/',
 					plugins: [imageminWebp({ quality: 70 })]
+				})
+			}
+		},
+		{
+			name: 'handlebars-hmr',
+			configureServer(server) {
+				const watchDirs = [
+					path.resolve(__dirname, 'src/data'),
+					path.resolve(__dirname, 'src/partials'),
+				]
+				watchDirs.forEach(dir => server.watcher.add(dir))
+				server.watcher.on('change', filePath => {
+					if (watchDirs.some(dir => filePath.startsWith(dir))) {
+						server.ws.send({ type: 'full-reload' })
+					}
 				})
 			}
 		}
